7428e224aa30a990ebe5377107f4768e4b1ffe01
Roberto <robertosilva1991@outlook.com>
2024-08-09 18:20:24 -0300

    nova implementação dos metodos getTask e getTasks no controller e service, além da criação do TaskNotFoundException.java

---
+++ src/main/java/com/study/task_manager/errors/TaskNotFoundException.java
@@ -0,0 +1,8 @@
+package com.study.task_manager.errors;
+
+public class TaskNotFoundException  extends RuntimeException{
+
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
+}
